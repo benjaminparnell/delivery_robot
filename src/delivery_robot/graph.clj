a6bde58dca21d7cf5ec41288ff01f34295099410
Ben Parnell <benjaminparnell.94@gmail.com>
2016-02-22 17:47:20 +0000

    can read a graph from a file into memory with weights

--- src/delivery_robot/graph.clj
+++ src/delivery_robot/graph.clj
@@ -1,4 +1,5 @@
-(ns delivery-robot.graph)
+(ns delivery-robot.graph
+  (:require [clojure.string :as str]))
 
 (defn create-node
   "create-node returns a hash map of a node name with no packages in it and
@@ -22,3 +23,35 @@
   (first
     (filter #(= (%1 :name) name) graph)))
 
+(defn parse-file
+  "parse-file converts text into a data structure load-from-file can read"
+  [file]
+  (map #(zipmap [:name :neighbours] %1)
+       (map #(map str/trim (str/split %1 #"->"))
+            (str/split file #"\n"))))
+
+(defn parse-neighbour-weight
+  "parse-neighbour-weight checks for an optional weight in the neighbours name"
+  [name]
+  (let [match (re-seq #"\w+" name)]
+    (if (= (count match) 1)
+      (conj match 1)
+      (conj match (Integer. (nth match 1))))))
+
+(defn parse-neighbours
+  "parse-neighbours turns parse-file neighbours into coll of neighbours"
+  [node]
+  (assoc
+    node
+    :neighbours
+    (map 
+      #(zipmap [:cost :name] (parse-neighbour-weight %1))
+      (str/split (node :neighbours) #", "))))
+
+(defn load-from-file
+  "load-from-file loads a graph from a file"
+  [path]
+  (let [file (parse-file (slurp path))]
+    (map #(assoc (parse-neighbours %1) :packages '()) file)))
+
+; (get-node "ts" (load-from-file "graph.txt"))
